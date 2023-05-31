631959e7f2b935cc2cdd3dde932b0280ecb04334
yangseongyeal2 <ysy96pashon@korea.ac.kr>
2023-05-31 15:23:35 +0900

    Created SwiftUI Project, gitignore

---
+++ iPexel/iPexel/iPexelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPexelApp.swift
+//  iPexel
+//
+//  Created by mobile_ on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct iPexelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
