77b9338162492ec990c96907e0ee50f5ef517975
junsuboy <junsuboyboy@hanyang.ac.kr>
2023-06-01 09:44:25 +0900

    Image entity

--- iPexel/iPexel/Data/Entity/Image.swift
+++ iPexel/iPexel/Data/Entity/Image.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Image: Codable {
+    let id: Int
+    let width: Int
+    let height: Int
+    let url: String
+    let photographer: String
+    let photographerUrl: String
+    let avgColor: String
+    let src: ImageSrc
+    let liked: Bool
+    let alt: String
+}
+
+struct ImageSrc: Codable {
+    let original: String
+    let large2x: String
+    let large: String
+    let medium: String
+    let small: String
+    let portrait: String
+    let landscape: String
+    let tiny: String
+}
