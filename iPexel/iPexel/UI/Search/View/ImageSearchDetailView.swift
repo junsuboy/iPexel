ed8597678ea4bff9cc921e97b3a34f52e8c8e850
junsuboy <cobijraynor@naver.com>
2023-06-02 16:01:17 +0900

    이미지 저장 기능 추가

--- iPexel/iPexel/UI/Search/View/ImageSearchDetailView.swift
+++ iPexel/iPexel/UI/Search/View/ImageSearchDetailView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct ImageSearchDetailView: View {
+    @State var isShowAlert = false
+    
     var photo: ResponseImagePhoto
     
     var body: some View {
@@ -20,7 +23,32 @@ struct ImageSearchDetailView: View {
                     .progressViewStyle(CircularProgressViewStyle(tint: .black))
                     .scaleEffect(3)
             })
+            .onTapGesture {
+                isShowAlert = true
+            }
             Spacer()
         }
+        .alert("Save This Image", isPresented: $isShowAlert) {
+            Button("cancel", role: .cancel) {}
+            Button("save") {
+                downloadImage(url: photo.src.original)
+            }
+        }
+    }
+    
+    func downloadImage(url: String) {
+        let destination: DownloadRequest.Destination = { _, _ in
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let uuid = "\(UUID())"
+            let fileURL = documentsURL.appendingPathComponent(uuid + ".jpeg")
+
+            return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
+        }
+
+        AF.download(url, to: destination).response { response in
+            debugPrint(response)
+        }
+        
+        print("download complete!")
     }
 }
